97aa4b9df3e9226135d69b97778f4c002b7ed6d2
NickAc <32451103+NickAcPT@users.noreply.github.com>
2020-08-31 19:39:10 +0100

    Fix Components not serializing

--- craftlib-protocol/src/main/kotlin/dev/zerite/craftlib/protocol/util/ext/JsonExt.kt
+++ craftlib-protocol/src/main/kotlin/dev/zerite/craftlib/protocol/util/ext/JsonExt.kt
@@ -4,10 +4,11 @@ package dev.zerite.craftlib.protocol.util.ext
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import dev.zerite.craftlib.protocol.util.json.factory.ProtocolVersionTypeAdapter
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer
 
 /**
  * Reference to the singular gson instance which we use.
  */
-val gson: Gson = GsonBuilder()
+val gson: Gson = GsonComponentSerializer.gson().serializer().newBuilder()
     .registerTypeAdapterFactory(ProtocolVersionTypeAdapter)
     .create()
